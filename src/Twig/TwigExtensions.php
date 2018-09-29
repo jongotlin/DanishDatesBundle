cf2b04fb51ee40e60e091fb669d01121963f46e1
Jon Gotlin <jon@jon.se>
2018-09-29 22:11:33 +0200

    Added danish date name filter

--- src/Twig/TwigExtensions.php
+++ src/Twig/TwigExtensions.php
@@ -2,6 +2,18 @@
 
 namespace JGI\DanishDatesBundle\Twig;
 
+use Twig\TwigFilter;
+
 class TwigExtensions extends \JGI\SwedishDatesBundle\Twig\TwigExtensions
 {
+    /**
+     * @return TwigFilter[]
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('danish_date_name', [$this, 'dateName']),
+            new TwigFilter('holiday', [$this, 'holiday']),
+        ];
+    }
 }
